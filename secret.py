f054fc51ae044eb8195020a3a9129df234e06430
MerlanFriX <107347267+MerlanFriX@users.noreply.github.com>
2025-01-05 11:35:53 +0100

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,12 @@
+ACCOUNTS = {
+    "bitget1": {
+        "public_api": "",
+        "secret_api": "",
+        "password": "",
+    },
+    "bitmart1": {
+        "public_api": "",
+        "secret_api": "",
+        "memo": "",
+    },
+}
